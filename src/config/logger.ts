e6154d14e9afd9c1834140ec686ab9016a4777e1
Athul-13 <kkunnikrishnan13@gmail.com>
2026-01-14 09:21:01 +0530

    feat: enhance logger configuration with timestamp formatting and custom output structure

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -5,16 +5,19 @@ import winston from 'winston'
  */
 export const logger = winston.createLogger({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  format: winston.format.combine(
-    winston.format.timestamp(),
+  format: winston.format.combine(    
     winston.format.errors({ stack: true }),
-    winston.format.json()
+    winston.format.json(),
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' })
   ),
   transports: [
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.colorize(),
-        winston.format.simple()
+        winston.format.printf(({level, message, timestamp, ...meta }) => {
+          const metaString = Object.keys(meta).length ? JSON.stringify(meta) : ''
+          return `${timestamp} [${level}]: ${message} ${metaString}`
+        })
       ),
     }),
   ],
